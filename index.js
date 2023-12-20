89b654c0f735d9912c9ad48d73b8a27f8ac31b8f
Toria Sellers <victoria.12292001@gmail.com>
2023-12-20 09:43:01 -0500

    console toria

--- index.js
+++ index.js
@@ -23,3 +23,4 @@
     // Creating a variable for integer then .string the integer
     // parseInt(num.toString().split('').map(arrow function).join
     // return parseInt
+    console.log("toria")
